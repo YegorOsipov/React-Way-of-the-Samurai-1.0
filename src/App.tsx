f5e9e3d284c445d7b9fc1e3a291ef7dab779275b
Egor <gogsyan11@gmail.com>
2021-11-05 21:18:07 +0300

    Урок7

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header />
+        <Technologies />
     </div>
   );
+}
+
+function Technologies() {
+    return (
+      <div>
+          <ul>
+              <li>html</li>
+              <li>css</li>
+              <li>js</li>
+              <li>react</li>
+          </ul>
+      </div>
+    );
+}
+
+function Header() {
+    return (
+        <div>
+            <a href={'#'}>Home</a>
+            <a href={'#'}>News Feed</a>
+            <a href={'#'}>Messages</a>
+        </div>
+    )
 }
 
 export default App;
